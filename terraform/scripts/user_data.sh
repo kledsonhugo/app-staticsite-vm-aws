156b37c558c137c173c4e002b52bd5023de3b3cd
Caio Martins Silveira <caiom.silveira30@gmail.com>
2023-04-06 12:21:03 -0300

    caio-martins-silveira

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -10,9 +10,23 @@ echo "Enable Apache service to start after reboot"
 sudo systemctl enable httpd
     
 echo "Install application"
-cd /tmp
-git clone https://github.com/kledsonhugo/app-static-site-ec2
-cp /tmp/app-static-site-ec2/app/*.html /var/www/html/
+cat <<EOF > /var/www/html/index.html
+<html>
+    <head>
+        <title>S3 Site</title>
+        <meta http-equiv=Content-Type content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <p>Olá. Me chamo Caio e não sei nada de HTML</p>
+        <p>Mas faz parte</p>
+        <p></p>
+        <p>Acesse o Insta e me add.</p>
+        <p>c.martinsz</p>
+        <a href="https://www.instagram.com/"</a>
+
+    </body>
+</html>
+EOF
     
 echo "Start Apache service"
 service httpd restart
