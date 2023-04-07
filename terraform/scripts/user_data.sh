000aed5835184fda3c7e7c8a0d035be78885a209
biaassuncao <biaassuncao0307@gmail.com>
2023-04-07 03:07:50 -0300

    pull request bia

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -13,6 +13,23 @@ echo "Install application"
 cd /tmp
 git clone https://github.com/kledsonhugo/app-static-site-ec2
 cp /tmp/app-static-site-ec2/app/*.html /var/www/html/
+cat <<EOF > /var/www/html/index.html
+<html>
+    <head>
+        <title>ola professor</title>
+        <meta http-equiv=Content-Type content="text/html; charset=utf-8">
+    </head>
+    <body>
+        <p>voce sabia</p>
+        <p>que</p>
+        <p>eu estou</p>
+        <p>sofrendo muito</p>
+        
+        <p>na sua materia? rsrs</p>
+        
+    </body>
+</html>
+EOF
     
 echo "Start Apache service"
 service httpd restart
