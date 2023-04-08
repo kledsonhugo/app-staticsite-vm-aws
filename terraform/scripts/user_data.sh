085db8ca1100cb644526d2ddf02a25c04eeac8ce
NaderSouza <naderhadadsouza@gmail.com>
2023-04-08 03:10:32 -0300

    Checkpoint 02

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-    
+
 echo "Update with latest packages"
 yum update -y
-    
+
 echo "Install Apache"
 yum install -y httpd git
-    
+
 echo "Enable Apache service to start after reboot"
 sudo systemctl enable httpd
-    
+
 echo "Install application"
 cd /tmp
-git clone https://github.com/kledsonhugo/app-static-site-ec2
+# git clone https://github.com/kledsonhugo/app-static-site-ec2
+git clone -b updateapp https://github.com/NaderSouza/app-static-site-ec2
 cp /tmp/app-static-site-ec2/app/*.html /var/www/html/
-    
+
 echo "Start Apache service"
 service httpd restart
