4df5327b1565d3a852d67a88badd60c11b8a8d60
Victor Lopes <rm550401@outlook.com.br>
2023-04-09 21:09:40 -0300

    Checkpoint 2

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -10,9 +10,29 @@ echo "Enable Apache service to start after reboot"
 sudo systemctl enable httpd
     
 echo "Install application"
-cd /tmp
-git clone https://github.com/kledsonhugo/app-static-site-ec2
-cp /tmp/app-static-site-ec2/app/*.html /var/www/html/
+cd var/www/html/
+touch index.html
+echo -e "
+<html>\n
+    <head>\n
+        <title>S3 Site</title>\n
+        <meta http-equiv=Content-Type content=\"text/html; charset=utf-8\">\n
+    </head>\n
+    <body>\n
+        <p>Olá.</p>\n
+        <p>Bem vindo !!!!!!!!</p>\n
+        <p></p>\n
+        <p>Aqui você irá:</p>\n
+        <ul>\n
+        <li>aprender</li>\n
+        <li>colaborar</li>\n
+        <li>compartilhar</li>\n
+        </ul>\n
+        <p><b>Kledson Basso</b><br /><a href=\"https://www.linkedin.com/in/kledsonbasso/\">https://www.linkedin.com/in/kledsonbasso/</a></p>\n
+        <p>Victor Lopes</p>\n
+    </body>\n
+</html>\n
+" >> index.html
     
 echo "Start Apache service"
 service httpd restart
