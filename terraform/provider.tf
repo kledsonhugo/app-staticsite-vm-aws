15ca2ebd3eb40e5b3ffa230ab4b17c252e54ad89
Kledson Basso <kledsonhugo@gmail.com>
2023-04-12 22:20:58 -0300

    Checkpoint 02

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,5 +16,5 @@ provider "aws" {
   region                   = "us-east-1"
   shared_config_files      = ["./.aws/config"]
   shared_credentials_files = ["./.aws/credentials"]
-  profile                  = "fiap"
+  profile                  = "titio-eh-foda"
 }
