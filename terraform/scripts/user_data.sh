bd4c0ff5665187498bf891017c6866d383c3f0a8
Lurickardo <luizr726@gmail.com>
2023-04-17 23:01:44 -0300

    Add update whithout stop application

--- terraform/scripts/user_data.sh
+++ terraform/scripts/user_data.sh
@@ -11,8 +11,20 @@ sudo systemctl enable httpd
     
 echo "Install application"
 cd /tmp
-git clone https://github.com/kledsonhugo/app-static-site-ec2
+git clone https://github.com/lurickardo/app-static-site-ec2
 cp /tmp/app-static-site-ec2/app/*.html /var/www/html/
+cat <<EOF > /var/www/html/index.html
+<html>
+    <head>
+        <title>Site Luiz Ricardo | RM 98150</title>
+    </head>
+    <body>
+        <p>Kledsoooooooooooooooooooon</p>
+        <p>Gostaria de mostrar para você Davi Jones, o inocente mais culpado do mundo:</p>
+        <img src="https://tm.ibxk.com.br/2022/09/16/16183746604197.jpg" alt="Dava jonas">
+    </body>
+</html>
+EOF
     
 echo "Start Apache service"
 service httpd restart
