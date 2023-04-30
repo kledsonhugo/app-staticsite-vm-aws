e83aedd73a39edc78ccb789a9c3c83bedb05511e
SallesAnderson <salllesnader@btech.info>
2023-04-29 22:44:16 -0300

    modificando página index.html

---
+++ terraform/connection.tf
@@ -0,0 +1,31 @@
+resource "aws_key_pair" "ec2_key_pair" {
+  key_name   = "ec2_key_pair"
+  public_key = file("/c/Users/vmart/.ssh/id_rsa.pub")
+}
+
+resource "null_resource" "front_end_apply" {
+  provisioner "remote-exec" {
+    inline = [
+      "if [ -d /var/www/html ]; then",
+      "    rm -rf /tmp/repo-atualizado || echo ''",
+      "    git clone https://github.com/Salless/app-static-site-ec2 /tmp/repo-atualizado",
+      "    sudo cp -r /tmp/repo-atualizado/app/* /var/www/html/",
+      "    sudo systemctl restart httpd",
+      "    echo 'Site atualizado!'",
+      "else",
+      "    echo 'Ainda não rodou o script de user-data'",
+      "fi"
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    host        = aws_instance.instance.public_ip
+    private_key = file("/c/Users/vmart/.ssh/id_rsa")
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
+  }
+}
--- terraform/main.tf
+++ terraform/main.tf
@@ -82,4 +82,5 @@ resource "aws_instance" "instance" {
     subnet_id              = aws_subnet.sn_public.id
     vpc_security_group_ids = [aws_security_group.sg_public.id]
     user_data              = "${base64encode(data.template_file.user_data.rendered)}"
+    key_name = "ec2_key_pair"
 }
