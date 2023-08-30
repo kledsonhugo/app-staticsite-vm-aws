4230ce2aefb4e7014709359f5f232bdb0a0c6fb4
Ricardo Lima <rco3000@hotmail.com>
2023-08-30 20:08:00 -0300

    ricardolima_30-08-2023

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,19 @@
-# PROVIDER
 terraform {
-
-  required_version = "~> 1.4"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.55"
+      version = "~> 5.0"
     }
   }
 
+  backend "s3" {
+    bucket         = "terraform-state-do-ricardolima"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock-do-ricardolima"
+  }
 }
 
 provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = ["./.aws/config"]
-  shared_credentials_files = ["./.aws/credentials"]
-  profile                  = "fiap"
+  region = var.aws_region
 }
