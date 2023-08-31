48ea0543d9179a2ea617a0a05ff0a1ddd73c0aeb
Henrique Francisco <henriaf@outlook.com>
2023-08-30 22:19:41 -0300

    foof

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,27 +1,23 @@
-# PROVIDER
 terraform {
-
-  required_version = "~> 1.5.6"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
   }
+
   backend "s3" {
-    bucket         = "terraform-state-hike1"
+    bucket         = "terraform-state-hike"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-hike1"
+    dynamodb_table = "terraform-state-lock-hike"
     
 
   }
 }
-
 provider "aws" {
   region                   = "us-east-1"
-  shared_config_files      = ["./.aws/config"]
-  shared_credentials_files = ["./.aws/credentials"]
+  shared_config_files      = [".aws/config"]
+  shared_credentials_files = [".aws/credentials"]
   profile                  = "default"
 }
