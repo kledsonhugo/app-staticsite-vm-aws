8d74f903fe1509bd71c24c8b08d92903b0b19fed
Henrique Francisco <henriaf@outlook.com>
2023-08-30 22:28:59 -0300

    bolsonaro

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,16 +1,21 @@
+# PROVIDER
 terraform {
+
+  required_version = "~> 1.4"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0"
+      version = "~> 4.55"
     }
   }
 
+
   backend "s3" {
-    bucket         = "terraform-state-hike"
+    bucket         = "terraform-state-check-hike1"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-state-lock-hike"
+    dynamodb_table = "terraform-state-lock-check-hike1"
     
 
   }
